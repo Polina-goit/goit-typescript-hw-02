a57f37f4bf664e86be778fbedd8c52b0710d1de5
Polina <polinakoctyu4enko@gmail.com>
2024-11-19 17:59:03 +0200

    Update 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { getImages } from "../images-api";
 import { useEffect, useState } from "react";
-import Modal from "react-modal";
+// import Modal from "react-modal";
 import "modern-normalize";
 import toast, { Toaster } from "react-hot-toast";
 import css from "../components/App.module.css";
@@ -21,55 +21,51 @@ function App() {
   const [errorMessage, setErrorMessage] = useState(false);
   const [loading, setLoading] = useState(false);
   const [loadingMore, setLoadingMore] = useState(false);
-  const [isSearching, setIsSearching] = useState(false);
+
   const [selectedImage, setSelectedImage] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleSearch = async (searchQuery) => {
-    try {
-      setLoading(true);
-      setIsSearching(true);
-      setImages([]);
-      setPage(1);
-      setSearch(searchQuery);
-      const dataImg = await getImages(searchQuery, 1);
-      setTotalPages(dataImg.total_pages);
-      setImages(dataImg.results);
-
-      if (!dataImg.total) {
-        toast(
-          "Sorry, we have not found the photos for your request. Try to write it differently.",
-          {
-            duration: 2000,
-          }
-        );
-      } else {
-        toast.success(`We found ${dataImg.total} pictures`);
-      }
-    } catch {
-      setErrorMessage(false);
-      toast.error("The search field cannot be empty!");
-    } finally {
-      setLoading(false);
-      setIsSearching(false);
-    }
+    setImages([]);
+    setPage(1);
+    setSearch(searchQuery);
   };
 
   const handleLoadMore = async () => {
-    try {
-      setLoadingMore(true);
-      const nextPage = page + 1;
-      const dataImages = await getImages(search, nextPage);
-      setImages((prevImages) => {
-        return [...prevImages, ...dataImages.results];
-      });
-      setPage(nextPage);
-    } catch (errorMessage) {
-      setErrorMessage(true);
-    } finally {
-      setLoadingMore(false);
-    }
+    setPage(page + 1);
   };
+
+  useEffect(() => {
+    if (!search) return;
+    const fetchImages = async () => {
+      try {
+        setLoading(true);
+        setErrorMessage(false);
+
+        const dataImages = await getImages(search, page);
+
+        if (!dataImages.total) {
+          toast(
+            "Sorry, we have not found the photos for your request. Try to write it differently.",
+            {
+              duration: 2000,
+            }
+          );
+          return;
+        }
+        toast.success(`We found ${dataImages.total} pictures`);
+        setImages((prevImages) => {
+          return [...prevImages, ...dataImages.results];
+        });
+        setTotalPages(dataImages.total_pages);
+      } catch {
+        setErrorMessage(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchImages();
+  }, [page, search]);
 
   const isVisible = () => {
     return totalPages !== 0 && totalPages !== page && !loadingMore;
@@ -88,6 +84,10 @@ function App() {
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+      <ImageGallery imageList={images} openModal={openModal} />
+      {!loading && page < totalPages && (
+        <LoadMoreBtn onClick={handleLoadMore} isVisible={isVisible} />
+      )}
       <Toaster
         position="top-right"
         reverseOrder={false}
@@ -97,10 +97,7 @@ function App() {
       />
       {loading && <Loader />}
       {errorMessage && <ErrorMessage />}
-      <ImageGallery imageList={images} openModal={openModal} />
-      {!loadingMore && !isSearching && (
-        <LoadMoreBtn onClick={handleLoadMore} isVisible={isVisible} />
-      )}
+
       {loadingMore && <LoaderMore />}
       <ImageModal
         isOpen={modalIsOpen}
